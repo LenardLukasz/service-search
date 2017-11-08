c4c3c2c5c2114704a27cbe6e5e8832b05f5d513c
Lukasz Lenard <deckardless@gmail.com>
2017-11-08 17:32:22 +0100

    Added FXML stuff.

--- src/main/java/sample/controllers/MainController.java
+++ src/main/java/sample/controllers/MainController.java
@@ -1,15 +1,31 @@
 package sample.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import sample.models.services.PizzaService;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable{
+
+    @FXML
+    Label labelText;
+
+    @FXML
+    TextField inputCity;
+
+    @FXML
+    Button buttonShowPizzerias;
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         PizzaService makeCallProbe = new PizzaService();
         makeCallProbe.makeCall("szewska+Cracow");
+
+        buttonShowPizzerias.
     }
 }
