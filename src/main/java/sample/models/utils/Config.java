64e3a506b28165e29bc06c914c2a8e47fcbcc138
Lukasz Lenard <deckardless@gmail.com>
2017-11-26 13:16:59 +0100

    Initial commit

---
+++ src/main/java/sample/models/utils/Config.java
@@ -0,0 +1,6 @@
+package sample.models.utils;
+
+public class Config {
+    public static final String APP_ID = "AIzaSyBBpECRKBjdA4g8bn4M5MAxLd2kLwF5fz8";
+    public static final String APP_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
+}
